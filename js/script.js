5a153c2cb7d5fc8a9bf5588380f3c7132ac86fde
eemilyy <emilyalmeeidas@gmail.com>
2021-10-14 21:17:32 -0300

    fixing the conditional

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ var image = document.getElementById('img')
 
 var data = new Date()
 
-var mes = 12
-var dia = 25
+var mes = 9
+var dia = 23
 
 /*if(((mes + 1) >= 3 && dia >= 21) && ((mes + 1) <= 6 && dia < 21)){
     season.innerText = ('Autumn')
@@ -20,83 +20,79 @@ else if(((mes + 1) >= 12 && dia >= 21) && ((mes + 1) <= 3 && dia < 21)){
 }*/
 
 function summer_season(){
-    image.src = "images/summer.jpg"
     document.body.classList.add('summer-class')
+    image.src = "images/summer.jpg"
     season.innerText = ('Summer')
 }
 
 function autumn_season(){
-    image.src = "images/autumn.jpg"
     document.body.classList.add('autumn-class')
+    image.src = "images/autumn.jpg"
     season.innerText = ('Autumn')
 }
 
 function winter_season(){
-    image.src = "images/winter.jpg"
     document.body.classList.add('winter-class')
+    image.src = "images/winter.jpg"
     season.innerText = ('Winter')
 }
 
 function spring_season(){
-    image.src = "images/spring.jpg"
     document.body.classList.add('spring-class')
+    image.src = "images/spring.jpg"
     season.innerText = ('Spring')
 }
 
 if((mes) >= 3 && (mes) < 6){ //march, april, may, june
-    autumn_season()
     if((mes) == 3){
-        if(dia >= 21){
-            season.innerText = ('Autumn')
-        }
-        else{
-            summer_season()
-        }
-    }
-    else{
-        season.innerText = ('Autumn')
-    }
-}
-else if((mes) >= 6 && (mes) < 9){ //june, august, september
-    winter_season()
-    if((mes) == 6){
-        if(dia >= 21){
-            season.innerText = ('Winter')
+        if(dia < 21){
+            summer_season()          
         }
         else{
             autumn_season()
         }
     }
     else{
-        season.innerText = ('Winter')
+        autumn_season()
+    }
+}
+else if((mes) >= 6 && (mes) < 9){ //june, august, september
+    if((mes) == 6){
+        if(dia < 21){
+            autumn_season()
+        }
+        else{
+            winter_season()
+        }    
+    }
+    else{
+        winter_season()
     }
 }
 else if((mes) >= 9 && (mes) < 12){ //september, october, november, december
-    spring_season()
     if((mes) == 9){
         if(dia >= 23){
-            season.innerText = ('Spring')
+            spring_season()
         }
         else{
             winter_season()
         }
     }
     else{
-        season.innerText = ('Spring')
+        spring_season()
     }
 }
 else{ //december, january, february, march
-    summer_season()
     if((mes) == 12){
-        if(dia >= 21){
-            season.innerText = ('Summer')
+        if(dia < 21){
+            spring_season()            
         }
         else{
-            spring_season()
+            summer_season()
         }
     }
     else{
-        season.innerText = ('Summer')
+        summer_season()
     }
 }
 
