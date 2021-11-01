f490e18008e8d215869ad70633c6bbff7f53abd1
eemilyy <emilyalmeeidas@gmail.com>
2021-11-01 00:55:18 -0300

    erasing dead code

--- js/script.js
+++ js/script.js
@@ -112,21 +112,3 @@ else{ //december, january, february, march
         summer_season()
     }
 }
-
-
-//const season_nadata.getMonth() + 1 = ["Autumn", "Summer", "Spring", "Winter"];
-
-const month = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December"
-}
